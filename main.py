70a4818a9c59399db8691808aef9732be33ea81b
tkTeamShiny <71694626+tkTeamShiny@users.noreply.github.com>
2025-09-19 06:35:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import os
 import time
 import random
 import numpy as np
-import tensorflow as _tf
-_tf.compat.v1.disable_v2_behavior()   # ← 先に無効化
-tf = _tf.compat.v1                    # ← その後、"tf" を v1 に差し替え
+import tensorflow as tf
 
 from net import *  # Network, data_loader_bd_rm_from_tfrecord, GPU_ID などを想定
 from scores import fast_hist
@@ -368,7 +366,7 @@ class MODEL(Network):
 
 
 def main(args):
-    tf.set_random_seed(seed)
+    tf.random.set_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
 
